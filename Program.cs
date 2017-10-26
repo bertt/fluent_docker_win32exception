49d4802256db0d2f97ae9396dd2048854a5c4195
bert <btemme@hotmail.com>
2017-10-26 10:46:24 +0200

    add initial code

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Ductus.FluentDocker.Services;
+using System;
+
+namespace fluent_docker_win32exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hosts = new Hosts().Discover();
+            Console.WriteLine("Number of hosts:" + hosts.Count);
+            Console.ReadKey();
+        }
+    }
+}
