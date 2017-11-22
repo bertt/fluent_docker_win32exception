9905420f9325825c84165d3576c73274af41b418
bert <btemme@hotmail.com>
2017-11-22 11:20:37 +0100

    upgrade packages

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Ductus.FluentDocker.Services;
+﻿using Ductus.FluentDocker.Extensions.Utils;
+using Ductus.FluentDocker.Services;
 using System;
 
 namespace fluent_docker_win32exception
@@ -7,6 +8,12 @@ namespace fluent_docker_win32exception
     {
         static void Main(string[] args)
         {
+            
+            var r = new DockerBinariesResolver();
+            Console.WriteLine("docker        : " + (r.MainDockerClient.FqPath ?? "not found"));
+            Console.WriteLine("docker-compose: " + (r.MainDockerCompose.FqPath ?? "not found"));
+            // Console.WriteLine("docker-machine: " + (r.MainDockerMachine.FqPath ?? "not found"));
+
             var hosts = new Hosts().Discover();
             Console.WriteLine("Number of hosts:" + hosts.Count);
             Console.ReadKey();
